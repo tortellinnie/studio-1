2debe7deed36ddae5bbbcfd57ec3c7c69ec4f053
SHANN KARL FELIPE <snfelipe@fit.edu.ph>
2026-03-15 11:31:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1789396782-ace42",
+  "appId": "1:214240082943:web:a9712bb1bf86df66ed05ca",
+  "apiKey": "AIzaSyB67rKVu4cucuI9jqQmRQmrq7bd5lwY8V0",
+  "authDomain": "studio-1789396782-ace42.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "214240082943"
+};
